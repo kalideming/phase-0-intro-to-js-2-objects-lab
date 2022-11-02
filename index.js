8e95bd446761c77bc46435bd4f4b1f1559542d4e
Kali Deming <kalideming@icloud.com>
2022-11-02 16:22:57 -0600

    commit tree c58b3e2bf1498d919ab0550319bf4d6598fbdd61

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
-// Write your solution in this file!
+const employee = {
+    name : 'Sam',
+    streetAddress : '11 Broadway'
+};
+
+beforeEach(function () {
+    for (const key in employee) {
+      delete employee[key];
+    }
+
+    employee.name = 'Sam';
+  });
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key]: value,
+    };
+  };
+
+  beforeEach(function () {
+    for (const key in employee) {
+      delete employee[key];
+    }
+
+    employee.name = 'Sam';
+  });
+
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  };
+
+  beforeEach(function () {
+    for (const key in employee) {
+      delete employee[key];
+    }
+
+    employee.name = 'Sam';
+  });
+
+function deleteFromEmployeeByKey(employee, key) {
+   const newObj = {...employee};
+   delete newObj[key];
+   return newObj;
+};
+
+beforeEach(function () {
+    for (const key in employee) {
+      delete employee[key];
+    }
+
+    employee.name = 'Sam';
+  });
+
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  };
